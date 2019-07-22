a94756966476585980a8ef023d2cad6c29b4b6c3
kluwena <karelluwena@gmail.com>
2019-07-22 19:42:09 +0700

    http server init

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,1 +1,35 @@
-package server
+package http
+
+import (
+	"github.com/go-chi/chi"
+	"encoding/json"
+	// "log"
+	// "net/http"
+	// "context"
+	"github.com/kluwena/go-api-practice/internal/order"
+)
+
+// Server represents the http server of order
+type Server struct {
+	router chi.Router
+
+	orderService order.ServiceInterface
+}
+
+
+// NewServer create a new order http server
+func NewServer(
+	orderService order.serviceInterface,
+) *Server {
+	s:= &Server{
+		router: chi.NewRouter(),
+		orderService: orderService,
+	}
+	s.buildRoutes()
+	return s
+}
+
+// ServeHTTP serves the http requests
+func (s *Server) serveHTTP (w http.ResponseWriter r *http.Request) {
+	s.router.serveHTTP(w, r)
+}
